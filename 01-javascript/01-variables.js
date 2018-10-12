da851e16388d24d3c63885e19d5673b51f73196a
Israel Pérez <israel-03j@hotmail.com>
2018-10-12 17:48:24 -0500

    hasta if then else

--- 01-javascript/01-variables.js
+++ 01-javascript/01-variables.js
@@ -7,9 +7,50 @@ var edadString = "10"
 var sueldo = 1.344
 var casado = false
 var hijos = null
+var dato;
+var fechadeNacimiento = new Date()
+var israel = {
+    "nombre": "Jose",
+    'segundoNombre': 'Israel',
+    apellidoPaterno: 'Perez',
+    apellidoMaterno: 'Espinosa',
+    edad: 29,
+    casado: false,
+    hijos: null,
+    mascotas: {nombre: 'firulais'}
+
+}
+
+
 console.log("Hola mundo")
 console.log('edad', typeof edad)// number
 console.log('edadString', typeof edadString)// String
 console.log('sueldo', typeof sueldo)// number
 console.log('casado', typeof casado)// boolean
 console.log('hijos', typeof hijos)// object
+console.log('dato valor', dato)// undefined
+console.log('dato', typeof dato)// undefined cuando se define un var sin valor
+console.log('fechedeNAcimiento', typeof fechadeNacimiento)// object
+console.log('israel', typeof israel)
+console.log(israel)
+console.log(israel.mascotas.nombre)
+israel.hija = {nombre: 'roromiya'}
+console.log(israel.hija)
+if(1){//truty
+    console.log('si')
+}else{
+    console.log('no')
+}
+if(0){//falsy
+    console.log('si')
+}else{
+    console.log('no')
+}
+
+if(-4){//truty
+    console.log('si')
+}else{
+    console.log('no')
+}
+
+
