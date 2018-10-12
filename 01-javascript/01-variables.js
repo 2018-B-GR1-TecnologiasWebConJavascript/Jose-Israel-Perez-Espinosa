f12a39c19b7da8b0ee0ac5bd3fbfdfc22a2460e6
Israel Pérez <israel-03j@hotmail.com>
2018-10-12 17:06:44 -0500

    variables

---
+++ 01-javascript/01-variables.js
@@ -0,0 +1,15 @@
+// java  int edad =10;
+// js es un lenguaje no tipado
+
+var edad = 10;
+//no se necesita usar comillas
+var edadString = "10"
+var sueldo = 1.344
+var casado = false
+var hijos = null
+console.log("Hola mundo")
+console.log('edad', typeof edad)// number
+console.log('edadString', typeof edadString)// String
+console.log('sueldo', typeof sueldo)// number
+console.log('casado', typeof casado)// boolean
+console.log('hijos', typeof hijos)// object
